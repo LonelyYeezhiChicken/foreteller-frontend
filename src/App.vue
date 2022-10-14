585042a1d760f06c2ef842a7b440729dc69067c0
chicken <hibary.taiwan@gmail.com>
2022-10-15 02:46:07 +0800

    feat: login page

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,29 @@
+
+<script  lang="ts">
+const rootEl = document.getElementById("app");
+export default {
+  name: "thisApp",
+  data() {
+    return {};
+  },
+  methods: {
+    changDark: function (): void {
+      const _self = this;
+      console.log(rootEl);
+      if (rootEl !== null) {
+        if (rootEl.className.includes("dark")) {
+          rootEl.classList.remove("dark");
+        } else {
+          rootEl.classList.add("dark");
+        }
+      }
+    },
+  },
+};
+</script>
 <template>
-  <router-view />
+  <main className="dark:bg-blueGray-700 font-mono bg-amber-100 h-screen">
+    <input type="button" value="變色" @click="changDark()" />
+    <router-view class="relative w-full h-full py-40 min-h-screen" />
+  </main>
 </template>
