ac623c415d759d6b23c0764291ff24791ce061f9
chicken <hibary.taiwan@gmail.com>
2022-10-15 02:46:59 +0800

    Updates

--- assets/Home.ca134a30.js
+++
@@ -1,1 +0,0 @@
-import{d as e,c as o,o as t}from"./index.eedfcfa7.js";const n={class:"text-3xl font-bold underline"},a=e({__name:"Home",setup(s){return(c,l)=>(t(),o("h1",n,"Hello world!"))}});export{a as default};
