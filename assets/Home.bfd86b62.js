292d3e7680bb051178dc8c605cc6fe7c855c1ecf
chicken <hibary.taiwan@gmail.com>
2022-10-22 15:58:55 +0800

    Updates

--- assets/Home.bfd86b62.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./index.a00fc818.js";import{b as t,o as n,e as s,f as a,F as r,j as c}from"./@vue.7c8d5d90.js";import"./vue-router.08f749c7.js";const l={name:"home",components:{}},m=c("h1",{class:"text-3xl font-bold underline"},"Hello world!",-1);function p(_,d,f,i,u,x){const e=t("layout");return n(),s(r,null,[a(e),m],64)}const b=o(l,[["render",p]]);export{b as default};
