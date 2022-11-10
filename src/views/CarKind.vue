cec050fba1641d7117d8b69f529eeafc6627963d
chicken <hibary.taiwan@gmail.com>
2022-11-10 23:30:51 +0800

    fix: 車種維護頁面 error

--- src/views/CarKind.vue
+++ src/views/CarKind.vue
@@ -11,7 +11,7 @@ export default {
   name: "home",
   created() {
     console.log("Car Kind");
-    apiCarKindGet(null).then((res) => {
+    apiCarKindGet().then((res) => {
       console.log(res);
     });
   },
