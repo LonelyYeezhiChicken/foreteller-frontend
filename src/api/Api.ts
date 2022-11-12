f3b0c8d184dcc3c25251c73fc9cbc121b75a8ab4
chicken <hibary.taiwan@gmail.com>
2022-11-12 12:19:28 +0800

    chore: update vue-tsc

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -16,20 +16,3 @@ export const apiClaims = () => {
     return req.get(`/claims`, null, undefined)
 }
 
-/** 車種資料維護*/
-// 查詢
-export const apiCarKindGet = (data: any) => {
-    return req.get(`/CarKind`, { ...data }, undefined)
-}
-// 新增
-export const apiCarKindPost = (data: any) => {
-    return req.post(`/CarKind`, { ...data }, undefined)
-}
-// 修改
-export const apiCarKindPut = (data: any) => {
-    return req.put(`/CarKind`, { ...data }, undefined)
-}
-// 刪除
-export const apiCarKindDelete = (data: any) => {
-    return req.delete(`/CarKind`, { ...data }, undefined)
-}
